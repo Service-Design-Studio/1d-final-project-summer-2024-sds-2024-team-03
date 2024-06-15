6c9bedf05d356fe3010c68750e111705b3214fbb
Adelaine <ilenhanako@gmail.com>
2024-06-15 23:58:41 +0800

    Remove react/node_modules

--- react/node_modules/@types/react-transition-group/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { default as config } from "react-transition-group/config";
-export { default as CSSTransition } from "react-transition-group/CSSTransition";
-export { default as SwitchTransition } from "react-transition-group/SwitchTransition";
-export { default as Transition, TransitionStatus } from "react-transition-group/Transition";
-export { default as TransitionGroup } from "react-transition-group/TransitionGroup";
