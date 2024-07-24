c69da48b58c3f846b16ff59fe5d53d0de9ab2ffb
Adelaine <ilenhanako@gmail.com>
2024-07-25 03:34:29 +0800

    logs upload page

--- react/src/components/UploadData/Logs.tsx
+++ react/src/components/UploadData/Logs.tsx
@@ -1,40 +1,51 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
-import ListItemAvatar from "@mui/material/ListItemAvatar";
-import Avatar from "@mui/material/Avatar";
-import ImageIcon from "@mui/icons-material/Image";
-import WorkIcon from "@mui/icons-material/Work";
-import BeachAccessIcon from "@mui/icons-material/BeachAccess";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+interface Log {
+  log_message: string;
+  created_at: string;
+}
 
 export default function FolderList() {
+  //
+  const [logs, setLogs] = useState<Log[]>([]);
+
+  useEffect(() => {
+    const urlPrefix =
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000"
+        : "https://jbaaam-yl5rojgcbq-et.a.run.app";
+    fetch(`${urlPrefix}/logs.json`)
+      .then((response) => response.json())
+      .then((data) =>
+        setLogs(
+          data.sort((a: { log_message: string }, b: { log_message: string }) =>
+            a.log_message.localeCompare(b.log_message)
+          )
+        )
+      );
+  }, []);
+  console.log(logs);
   return (
     <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <ImageIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Photos" secondary="Jan 9, 2014" />
-      </ListItem>
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <WorkIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Work" secondary="Jan 7, 2014" />
-      </ListItem>
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <BeachAccessIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Vacation" secondary="July 20, 2014" />
-      </ListItem>
+      {logs.map((log, index) => (
+        <ListItem>
+          <ListItemText
+            primary={log.log_message}
+            secondary={dayjs(log.created_at)
+              .tz("Asia/Singapore")
+              .format("YYYY-MM-DD HH:mm:ss")}
+          />
+        </ListItem>
+      ))}
     </List>
   );
 }
