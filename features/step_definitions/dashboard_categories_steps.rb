12f153143ba838b245aea52bfe824cf7bcdaf382
Bryan Tan <bryan10350@gmail.com>
2024-07-25 14:50:04 +0800

    fixed cucumber dashboard_category

--- features/step_definitions/dashboard_categories_steps.rb
+++ features/step_definitions/dashboard_categories_steps.rb
@@ -1,32 +1,32 @@
 And (/I should see 5 subcategories with the most positive sentiments/) do
-    parent_elements = all("g[transform='translate(250,10)']")  # Get all matching elements
-    parent_element = parent_elements.first  # Select the first element
-    expect(page).to have_content("null > Staff Related")
+    expect(page).to have_content("null > Staff Related") 
     expect(page).to have_content("United Trust (UT) products > Staff Related")
     expect(page).to have_content("NonUT products > Application Related")
     expect(page).to have_content("NonUT products > Fee Related")
     expect(page).to have_content("United Trust (UT) products > Application Related")
 end
 
-
-And (/with the 5 most positive sentiments sorted in descending order/) do
-    parent_elements = all("g[transform='translate(250,10)']")  # Get all matching elements
-    parent_element = parent_elements.first  # Select the first element
-    content = parent_element.text
-    expect(text).to match(/United Trust \(UT\) products > Application Related.*NonUT products > Application Related.*NonUT products > Fee Related.*United Trust \(UT\) products > Staff Related.*null > Staff Related/m)
-end
-
+And(/with the 5 most positive sentiments sorted in descending order/) do
+    parent_elements = all("g[transform='translate(250,10)']")
+    parent_element = parent_elements.first
+    
+    content = parent_element.text.gsub(/\s+/, ' ').strip  # Replace multiple spaces and newlines with a single space
+    
+    expected_text = [
+      'United Trust (UT) products > Application Related',
+      'NonUT products > Fee Related',
+      'NonUT products > Application Related',
+      'United Trust (UT) products > Staff Related',
+      'null > Staff Related'
+    ].join(' ')
+    
+    expect(content).to include(expected_text)
+end  
 
 When (/there is less than 5 categories in the category graph/) do
-    parent_elements = all("g[transform='translate(250,10)']")  # Get all matching elements
-    parent_element = parent_elements.first  # Select the first element
     expect(page).to have_content("null > Staff Related")
 end
-
 
 Then (/I should see only those categories/) do
-    parent_elements = all("g[transform='translate(250,10)']")  # Get all matching elements
-    parent_element = parent_elements.first  # Select the first element
     expect(page).to have_content("null > Staff Related")
 end
-    
