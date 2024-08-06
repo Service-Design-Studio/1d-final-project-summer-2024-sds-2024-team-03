aaee93d1cc9a95797b1b3137603839458b88b288
Adelaine <ilenhanako@gmail.com>
2024-08-06 20:28:03 +0800

    code cleanup2

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -7,44 +7,6 @@ class LogsController < ApplicationController
     render json: @logs
   end
 
-  # POST /logs or /logs.json
-  def create
-    @log = Log.new(log_params)
-
-    respond_to do |format|
-      if @log.save
-        format.html { redirect_to log_url(@log), notice: "Log was successfully created." }
-        format.json { render :show, status: :created, location: @log }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @log.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /logs/1 or /logs/1.json
-  def update
-    respond_to do |format|
-      if @log.update(log_params)
-        format.html { redirect_to log_url(@log), notice: "Log was successfully updated." }
-        format.json { render :show, status: :ok, location: @log }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @log.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /logs/1 or /logs/1.json
-  def destroy
-    @log.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to logs_url, notice: "Log was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_log
